ea64d1b6e3ee245efcb051befdecc3651e311a41
KolenCheung <christian.kolen@gmail.com>
2016-02-14 19:36:05 -0800

    Delete the scripts used to convert encoding to utf-8

--- encoding-big5-to-utf-8.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	iconv -f big5-2003 -t utf-8 "$line" > temp
-	sed -i '' s/big5/utf-8/ temp
-	mv -f temp "$line"
-done < "$1"
--- encoding-windows-1252-to-utf-8.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	iconv -f windows-1252 -t utf-8 "$line" > temp
-	sed -i '' s/windows-1252/utf-8/ temp
-	mv -f temp "$line"
-done < "$1"
